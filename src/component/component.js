394b9a0e91d44708958482c16e0ace0619f231f8
hyno97 <hyno@kyolimsoft.com>
2025-03-14 18:01:40 +0900

    2025_03_14 router-dom_v01

---
+++ src/component/component.js
@@ -0,0 +1,28 @@
+function Home() {
+    return (
+        <div>
+            <h2>Home</h2>
+            Home...
+        </div>
+    );
+}
+
+function Topics() {
+    return (
+        <div>
+            <h2>Topics</h2>
+            Topics...
+        </div>
+    );
+}
+
+function Contact() {
+    return (
+        <div>
+            <h2>Contact</h2>
+            Contact...
+        </div>
+    );
+}
+
+export { Home, Topics, Contact };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Home, Topics, Contact } from "./component/component";
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello React Router DOM</h1>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/Topics">Topics</Link></li>
+          <li><Link to="/Contact">Contact</Link></li>
+        </ul>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/Topics" element={<Topics />}></Route>
+          <Route path="/Contact" element={<Contact />}></Route>
+          <Route path="/*" element={'Not found'}></Route>
+        </Routes>
+      </div>
+    </>
+  );
+}
+ReactDOM.createRoot(document.getElementById('root')).render(<BrowserRouter><App /></BrowserRouter>);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
